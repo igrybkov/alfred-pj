18652f33565fe2e74c90c95dfa65e4f8b7f02652
Illia Grybkov <illia@grybkov.com>
2024-02-05 10:56:46 -0600

    Allow configuring Jupyter notebooks separately

--- main.py
+++ main.py
@@ -12,12 +12,13 @@ sys.path.insert(1, os.path.abspath("./lib"))
 
 from lib import click
 
+logging.basicConfig(level=logging.DEBUG, format='[%(levelname)] %(message)s')
 
 class Logger(logging.Logger):
     def __init__(self, name):
         super().__init__(name)
         self.addHandler(logging.StreamHandler(sys.stderr))
-        self.setLevel(logging.DEBUG)
+        self.setLevel(logging.DEBUG if os.environ['alfred_debug'] else logging.INFO)
         self.log(logging.INFO, f"logger initiated")
 
 logger = Logger(__name__)
@@ -28,7 +29,6 @@ response = {
     "items": [],
     "variables": {}
 }
-
 
 class UsageData:
     def __init__(self):
@@ -83,12 +83,20 @@ class Editors:
         return self.default_editor
 
     def get_editors_from_environment(self, env_var_name, defaults):
-        editors = os.environ[env_var_name]
+        logger.debug(f"getting editors from environment variable {env_var_name}")
+        var_names = [env_var_name] if isinstance(env_var_name, str) else env_var_name
+        logger.debug(f"var_names: {var_names}")
+        editors = None
+        for var_name in var_names:
+            if var_name in os.environ:
+                editors = os.environ[var_name]
+                break
         if not editors:
             return defaults
         return map(lambda editor: editor.strip(), editors.lower().split(","))
 
     def determine_editor(self, path):
+        logger.debug(f"determining editor for {path}")
         # this project has existing vscode configuration
         # and no idea configuration, so we assume vscode is the default
         if not os.path.isdir(os.path.join(path, '.idea')) and os.path.isdir(os.path.join(path, '.vscode')):
@@ -101,6 +109,10 @@ class Editors:
         # it has composer file, so it's php
         if os.path.isfile(f"{path}/composer.json") or len(glob.glob(f"{path}/*.php")) > 0:
             return self.get_first_available_editor(self.get_editors_from_environment('EDITORS_PHP', ['phpstorm', 'idea', 'code']))
+
+        # it has jupyter notebook file, so it's jupyter
+        if len(glob.glob(f"{path}/*.ipnb")) > 0:
+            return self.get_first_available_editor(self.get_editors_from_environment(['EDITORS_JUPYTER', 'EDITORS_PYTHON'], ['pycharm', 'idea', 'code']))
 
         # it has requirements file, so it's python
         if os.path.isfile(f"{path}/requirements.txt") or os.path.isdir(f"{path}/.venv") or len(glob.glob(f"{path}/*.py")) > 0:
